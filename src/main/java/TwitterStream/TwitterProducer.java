372dda9ec8b428dded16652790c4c2eb8ea8acab
Someindra <sonu@instance-1.us-central1-a.c.caramel-park-305906.internal>
2021-02-27 05:38:29 +0000

    High throuhputsetting

--- src/main/java/TwitterStream/TwitterProducer.java
+++ src/main/java/TwitterStream/TwitterProducer.java
@@ -108,6 +108,11 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        //High throughput settings
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(1024*32));
+
         KafkaProducer<String, String> producer=new KafkaProducer<String, String>(properties);
         return producer;
     }
